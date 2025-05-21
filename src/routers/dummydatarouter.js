51e0c04472eac2d11dc14b0919cafdd3a86313da
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-21 18:30:24 +0530

    task; one comment to be make note off

--- src/routers/dummydatarouter.js
+++ src/routers/dummydatarouter.js
@@ -5,6 +5,7 @@ const PriceStructure = require("../models/servicesInformation/priceStructure");
 dummydataRouter.post("/temp", async (req, res) => {
   try {
     //682d5728bd678e6e5496d6ae
+    //make sure if serviceStatus=0, then no paidinformation to be added.
     const data = new ExpectedServiceCost({
       serviceDataId: "682d5728bd678e6e5496d6ae",
       jobList: await PriceStructure.find({}),
