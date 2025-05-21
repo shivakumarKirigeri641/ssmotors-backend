d6df8568722771a3fa8d56f918544cea05b5daed
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-21 19:01:46 +0530

    task:dummy dates & bikes need to be added.

--- src/routers/dummydatarouter.js
+++ src/routers/dummydatarouter.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const getRandomNumber = require("../utils/dummy/getRandomNumber");
 const dummydataRouter = express.Router();
-const ExpectedServiceCost = require("../models/servicesInformation/expectedServiceCost");
-const PriceStructure = require("../models/servicesInformation/priceStructure");
 dummydataRouter.post("/temp", async (req, res) => {
   try {
-    //682d5728bd678e6e5496d6ae
-    //make sure if serviceStatus=0, then no paidinformation to be added.
-    const data = new ExpectedServiceCost({
-      serviceDataId: "682d5728bd678e6e5496d6ae",
-      jobList: await PriceStructure.find({}),
-    });
-    await data.save();
+    let arrayOfTwoWheelers = [];
+    let dayindex = 0;
+    const startDate = new Date("2023-01-01");
+    const today = new Date();
+
+    while (startDate <= today) {
+      //const countOfTwoWheelers = getRandomNumber(8, 18);
+      //for (let i = 0; i < countOfTwoWheelers; i++) {}
+      startDate.setDate(startDate.getDate() + 1);
+    }
+
     res.status(200).json({ status: "Ok", message: "success", data });
   } catch (err) {
     res.status(401).json({ status: "Failed", message: err.message });
