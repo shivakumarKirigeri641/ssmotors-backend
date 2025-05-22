689993d74aa03143013b1dfc0aba4c5b384b036b
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-22 19:02:11 +0530

    tasK; removed dummies

--- src/utils/dummy/getExpectedServiceCosts.js
+++
@@ -1,9 +0,0 @@
-const ExpectedServiceCost = require("../../models/servicesInformation/expectedServiceCost");
-const PriceStructure = require("../../models/servicesInformation/priceStructure");
-const getExpectedServiceCosts = async (serviceDataIdentification) => {
-  return new ExpectedServiceCost({
-    serviceDataId: serviceDataIdentification,
-    jobList: await PriceStructure.find({}),
-  });
-};
-module.exports = getExpectedServiceCosts;
