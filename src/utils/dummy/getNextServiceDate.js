abe96b403aba3c0b8d14a6e9023fd225bd4a5a28
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-22 11:20:36 +0530

    task: removed all dummy methods

--- src/utils/dummy/getNextServiceDate.js
+++
@@ -1,9 +0,0 @@
-const getNextServiceDate = (currentServiceDate) => {
-  const monthsToAdd = Math.random() < 0.5 ? 2 : 3;
-
-  const futureDate = new Date(
-    currentServiceDate.setMonth(currentServiceDate.getMonth() + monthsToAdd)
-  );
-  return futureDate;
-};
-module.exports = getNextServiceDate;
--- src/utils/dummy/getVariantId.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Twowheelervariants = require("../../models/twowheelervariants");
-const getRandomNumber = require("./getRandomNumber");
-const getVariantId = async () => {
-  const data = await Twowheelervariants.find({});
-  return data[getRandomNumber(0, data.length)]._id;
-};
-module.exports = getVariantId;
